4e3a05c46096b7f0fd585be016481b8535d32db7
Minyus <yusuke.minami86@gmail.com>
2020-08-16 17:42:42 +0800

    Parameterize; Update, Upgrade Airflow Chart to 7.4.1

--- setup.sh
+++ setup.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-kubectl create ns airflow
+airflow_chart_values="airflow_chart_values.yml"
+airflow_chart_version="7.4.1"
+airflow_ns="airflow"
+airflow_tasks_ns="airflow-tasks"
 
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo update
-helm install "airflow" stable/airflow --version "7.3.0" --namespace "airflow" --values helm_airflow_values.yml
+#############################################
 
-kubectl create ns airflow-tasks
-kubectl create role airflow-tasks -n airflow-tasks --verb=create,get,list --resource=pods,pods/log,pods/exec
-kubectl create rolebinding system-airflow-tasks -n airflow-tasks --role=airflow-tasks --serviceaccount=airflow:airflow 
-kubectl create sa airflow-tasks -n airflow-tasks
-kubectl create rolebinding airflow-tasks -n airflow-tasks --role=airflow-tasks --serviceaccount=airflow-tasks:airflow-tasks
+set -eu
+
+[[ ! -f "$airflow_chart_values" ]] && echo "$airflow_chart_values"" cannot be found." && exit 1
+
+kubectl create ns "$airflow_ns" 
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/ 
+helm repo update 
+helm install "airflow" "stable/airflow" --version "$airflow_chart_version" --namespace "$airflow_ns" --values "$airflow_chart_values" 
+for ns in $airflow_tasks_ns
+do
+kubectl create ns "$ns" 
+kubectl create role "airflow-tasks" -n "$ns" --verb=create,get,list --resource=pods,pods/log,pods/exec 
+kubectl create rolebinding "system-airflow-tasks" -n "$ns" --role="airflow-tasks" --serviceaccount="$airflow_ns":"airflow" 
+kubectl create sa "airflow-tasks" -n "$ns" 
+kubectl create rolebinding "airflow-tasks" -n "$ns" --role="airflow-tasks" --serviceaccount="$ns":"airflow-tasks"
+done
